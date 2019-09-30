c12e9dbefc332b06bfe222dbf838611f103a8c9e
yen880405 <55569670+yen880405@users.noreply.github.com>
2019-09-30 22:30:04 +0800

    Create simpleSort.py
    Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
    To understand how efficient the built-in Python sorting function is, you decided to implement your own simple sorting algorithm and compare its speed to the speed of the Python sorting. Write a function that, given an array of integers arr, sorts its elements in ascending order.
    
    Hint: with Python it's possible to swap several elements in a single line. To solve the task, use this knowledge to fill in both of the blanks (...).
    
    Example
    
    For arr = [2, 4, 1, 5], the output should be
    simpleSort(arr) = [1, 2, 4, 5].

---
+++ simpleSort.py
@@ -0,0 +1,13 @@
+def simpleSort(arr):
+
+    n = len(arr)
+
+    for i in range(n):
+        j = 0
+        stop = n - i
+        while j < stop - 1:
+            if arr[j] > arr[j + 1]:
+                arr = sorted(arr)
+            j += 1
+    return arr
+
