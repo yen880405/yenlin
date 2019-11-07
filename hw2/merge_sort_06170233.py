bab6bfcc742cf0ac8994057e4f7f2dc7c4930657
yen880405 <55569670+yen880405@users.noreply.github.com>
2019-11-08 02:11:14 +0800

    Create merge_sort_06170233.py

---
+++ hw2/merge_sort_06170233.py
@@ -0,0 +1,38 @@
+class MergeSort(object):
+
+    def __init__(self, array):
+        self.array = array
+
+    def merge_sort(self):
+        if len(self.array) > 1:
+            m = len(self.array)//2
+            left = self.array[:m]
+            right = self.array[m:]
+
+            leftsorter = MergeSort(left)
+            leftsorter.merge_sort()
+            rightsorter = MergeSort(right)
+            rightsorter.merge_sort()
+
+            i = 0
+            j = 0
+            k = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    self.array[k] = left[i]
+                    i += 1
+                else:
+                    self.array[k] = right[j]
+                    j += 1
+                k += 1
+
+            while i < len(left):
+                self.array[k] = left[i]
+                i += 1
+                k += 1
+
+            while j < len(right):
+                self.array[k] = right[j]
+                j += 1
+                k += 1
